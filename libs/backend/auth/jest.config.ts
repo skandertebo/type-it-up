053037ebba7012c2fbff04577fad297c066e2c8a
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-10-31 12:00:00 +0100

    fix: jest config fix

--- libs/backend/auth/jest.config.ts
+++ libs/backend/auth/jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   displayName: 'auth',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
