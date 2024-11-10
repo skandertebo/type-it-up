c9341815aae18bd251a126cb1339d99bebdc13ff
Skander Tebourbi <alexandertebourb@gmail.Com>
2024-11-10 12:00:00 +0100

    fix: jest config fix

--- libs/backend/auth/jest.config.ts
+++ libs/backend/auth/jest.config.ts
@@ -6,5 +6,5 @@ export default {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/backend',
+  coverageDirectory: '../../coverage/libs/backend/auth',
 };
