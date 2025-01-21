c0ff0604362a0a35b67f737af89820201f98219e
Amina Ghannem <amynaghannem@gmail.com>
2025-01-21 18:34:24 +0100

    create type it up profile lib

---
+++ libs/frontend/type-it-up-profile/src/lib/type-it-up-profile/type-it-up-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-type-it-up-profile',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './type-it-up-profile.component.html',
+  styleUrl: './type-it-up-profile.component.css',
+})
+export class TypeItUpProfileComponent {}
