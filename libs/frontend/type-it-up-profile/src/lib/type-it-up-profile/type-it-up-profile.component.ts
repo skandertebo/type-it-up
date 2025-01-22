2170d65fe66e119f856318ea4313f73bf11c835e
Amina Ghannem <amynaghannem@gmail.com>
2025-01-22 09:59:22 +0100

    feat: add user component

--- libs/frontend/type-it-up-profile/src/lib/type-it-up-profile/type-it-up-profile.component.ts
+++ libs/frontend/type-it-up-profile/src/lib/type-it-up-profile/type-it-up-profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TypeItUpUserComponent } from "../type-it-up-user/type-it-up-user.component";
 
 @Component({
   selector: 'lib-type-it-up-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TypeItUpUserComponent],
   templateUrl: './type-it-up-profile.component.html',
   styleUrl: './type-it-up-profile.component.css',
 })
