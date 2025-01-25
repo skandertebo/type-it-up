de8c21b6ddacbee8b8f449d3ab017f07eacee74e
Yasmine Kharrat <114816773+yasminekharrat123@users.noreply.github.com>
2025-01-25 17:56:28 +0100

    Revert "Login Component "

--- libs/frontend/type-it-up-landing/src/lib/login/login.component.ts
+++ libs/frontend/type-it-up-landing/src/lib/login/login.component.ts
@@ -1,31 +1,14 @@
-import { SVGDisplayComponent } from '@/frontend/shared';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lib-login',
   standalone: true,
   templateUrl: './login.component.html',
-  imports: [SVGDisplayComponent, ReactiveFormsModule],
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  loginForm: FormGroup;
-
-  constructor(private fb: FormBuilder) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-  }
-
-  onSubmit(): void {
-    if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-      console.log('Login Data:', { email, password });
-
-      alert('Login Successful!');
-    } else {
-      console.log('Form is invalid');
-    }
+  login() {
+    window.location.href =
+      'https://intimate-kaleidoscope-33-staging.authkit.app/';
   }
 }
