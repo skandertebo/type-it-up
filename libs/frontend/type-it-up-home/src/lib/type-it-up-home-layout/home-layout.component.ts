cd71662c9864d0e5c710a97ad8e5ee82b7e7429f
ykharrat <yasmine.kharrat@covectors.io>
2025-01-26 20:24:30 +0100

    feat: add login and logout buttons in layout + display username

--- libs/frontend/type-it-up-home/src/lib/type-it-up-home-layout/home-layout.component.ts
+++ libs/frontend/type-it-up-home/src/lib/type-it-up-home-layout/home-layout.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../type-it-up-navbar/navbar/navbar.component';
+import { AuthService } from '@/frontend/type-it-up-auth';
+import { combineLatest, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'lib-type-it-up-home-layout',
@@ -10,4 +12,22 @@ import { NavbarComponent } from '../type-it-up-navbar/navbar/navbar.component';
   templateUrl: './home-layout.component.html',
 })
 export class TypeItUpHomeLayoutComponent {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+
+  authState$: Observable<{ user: string | null; loading: boolean }> = combineLatest([
+    this.auth.user$,
+    this.auth.loading$,
+  ]).pipe(
+    map(([user, loading]) => ({ user: user?.username || null, loading }))
+  );
+
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  onLogout(): void {
+    this.auth.logout(); 
+    this.navigateToLogin();
+  }
 }
