a4a6fc6cb40a029c40ffe5eadef20644a1a5d7c5
Amina Ghannem <amynaghannem@gmail.com>
2025-01-26 19:40:39 +0100

    feat: test get user stats

--- libs/frontend/type-it-up-profile/src/lib/type-it-up-profile/type-it-up-profile.component.ts
+++ libs/frontend/type-it-up-profile/src/lib/type-it-up-profile/type-it-up-profile.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TypeItUpUserComponent } from "../type-it-up-user/type-it-up-user.component";
 import { LineChartComponent } from "../linechart/linechart.component";
+import { AuthService } from '@/frontend/type-it-up-auth';
+import { Apollo } from 'apollo-angular';
+import { GET_USER_STATS } from '@/frontend/type-it-up-graphql';
+import { get } from 'http';
 
 @Component({
   selector: 'lib-type-it-up-profile',
@@ -10,4 +14,24 @@ import { LineChartComponent } from "../linechart/linechart.component";
   templateUrl: './type-it-up-profile.component.html',
   styleUrl: './type-it-up-profile.component.css',
 })
-export class TypeItUpProfileComponent {}
+export class TypeItUpProfileComponent implements OnInit {
+  authService = inject(AuthService);
+  apollo = inject(Apollo);
+
+  ngOnInit() {
+    this.authService.user$.subscribe((user) => {
+      console.log(user);
+    });
+    this.getUserStats();
+  }
+
+  getUserStats() {
+    this.apollo
+      .query({
+        query: GET_USER_STATS,
+      })
+      .subscribe((result) => {
+        console.log(result);
+      });
+  }
+}
