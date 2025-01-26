66ee210850c4b7415f90087b2f11675d64e5b895
Amina Ghannem <amynaghannem@gmail.com>
2025-01-26 11:01:26 +0100

    fix: layout and apply filters only when click apply

--- libs/frontend/type-it-up-history/src/lib/type-it-up-history/type-it-up-history.component.ts
+++ libs/frontend/type-it-up-history/src/lib/type-it-up-history/type-it-up-history.component.ts
@@ -99,29 +99,32 @@ export class TypeItUpHistoryComponent implements OnInit {
     },
   ];
 
-  filters = {
-    sortBy: 'score', 
-    order: 'asc', 
+   filters = {
+    sortBy: 'score',
+    order: 'asc',
     fromDate: '',
     untilDate: '',
   };
 
+  appliedFilters = { ...this.filters };
+
   currentPage = 1;
   itemsPerPage = 10;
+
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
-      this.filters.sortBy = params['sortBy'] || 'score';
-      this.filters.order = params['order'] || 'asc';
-      this.filters.fromDate = params['fromDate'] || '';
-      this.filters.untilDate = params['untilDate'] || '';
+      this.appliedFilters.sortBy = params['sortBy'] || 'score';
+      this.appliedFilters.order = params['order'] || 'asc';
+      this.appliedFilters.fromDate = params['fromDate'] || '';
+      this.appliedFilters.untilDate = params['untilDate'] || '';
       this.currentPage = params['page'] ? +params['page'] : 1;
     });
   }
 
   get totalPages(): number {
-    return Math.ceil(this.gameHistory.length / this.itemsPerPage);
+    return Math.ceil(this.filteredData.length / this.itemsPerPage);
   }
 
   get paginatedData() {
@@ -132,24 +135,24 @@ export class TypeItUpHistoryComponent implements OnInit {
   get filteredData() {
     let data = [...this.gameHistory];
 
-    if (this.filters.fromDate) {
+    if (this.appliedFilters.fromDate) {
       data = data.filter(
-        (item) => new Date(item.date) >= new Date(this.filters.fromDate)
+        (item) => new Date(item.date) >= new Date(this.appliedFilters.fromDate)
       );
     }
-    if (this.filters.untilDate) {
+    if (this.appliedFilters.untilDate) {
       data = data.filter(
-        (item) => new Date(item.date) <= new Date(this.filters.untilDate)
+        (item) => new Date(item.date) <= new Date(this.appliedFilters.untilDate)
       );
     }
 
-    if (this.filters.sortBy === 'score') {
+    if (this.appliedFilters.sortBy === 'score') {
       data.sort((a, b) =>
-        this.filters.order === 'asc' ? a.score - b.score : b.score - a.score
+        this.appliedFilters.order === 'asc' ? a.score - b.score : b.score - a.score
       );
-    } else if (this.filters.sortBy === 'date') {
+    } else if (this.appliedFilters.sortBy === 'date') {
       data.sort((a, b) =>
-        this.filters.order === 'asc'
+        this.appliedFilters.order === 'asc'
           ? new Date(a.date).getTime() - new Date(b.date).getTime()
           : new Date(b.date).getTime() - new Date(a.date).getTime()
       );
@@ -159,17 +162,18 @@ export class TypeItUpHistoryComponent implements OnInit {
   }
 
   applyFilters() {
-    // Update the URL with the current filters
+    this.appliedFilters = { ...this.filters }; // Apply the current filter settings
+    this.currentPage = 1; // Reset to the first page
     this.router.navigate([], {
       relativeTo: this.route,
       queryParams: {
-        sortBy: this.filters.sortBy,
-        order: this.filters.order,
-        fromDate: this.filters.fromDate,
-        untilDate: this.filters.untilDate,
+        sortBy: this.appliedFilters.sortBy,
+        order: this.appliedFilters.order,
+        fromDate: this.appliedFilters.fromDate,
+        untilDate: this.appliedFilters.untilDate,
         page: this.currentPage,
       },
-      queryParamsHandling: 'merge', // Merge with existing params
+      queryParamsHandling: 'merge',
     });
   }
 
@@ -180,20 +184,28 @@ export class TypeItUpHistoryComponent implements OnInit {
       fromDate: '',
       untilDate: '',
     };
-    this.applyFilters(); // Update query params
+    this.applyFilters();
   }
-  
+
   nextPage() {
     if (this.currentPage < this.totalPages) {
       this.currentPage++;
-      this.applyFilters(); // Update query params
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { page: this.currentPage },
+        queryParamsHandling: 'merge',
+      });
     }
   }
 
   previousPage() {
     if (this.currentPage > 1) {
       this.currentPage--;
-      this.applyFilters(); // Update query params
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: { page: this.currentPage },
+        queryParamsHandling: 'merge',
+      });
     }
   }
 }
