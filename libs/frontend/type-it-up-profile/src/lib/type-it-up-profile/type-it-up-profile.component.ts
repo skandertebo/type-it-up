974154d9962d88d10066fb7af6fb675fd8c3e581
Amina Ghannem <amynaghannem@gmail.com>
2025-01-27 11:15:03 +0100

    feat: integrate user profile

--- libs/frontend/type-it-up-profile/src/lib/type-it-up-profile/type-it-up-profile.component.ts
+++ libs/frontend/type-it-up-profile/src/lib/type-it-up-profile/type-it-up-profile.component.ts
@@ -1,11 +1,14 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TypeItUpUserComponent } from "../type-it-up-user/type-it-up-user.component";
-import { LineChartComponent } from "../linechart/linechart.component";
+import { TypeItUpUserComponent } from '../type-it-up-user/type-it-up-user.component';
+import { LineChartComponent } from '../linechart/linechart.component';
 import { AuthService } from '@/frontend/type-it-up-auth';
 import { Apollo } from 'apollo-angular';
-import { GET_USER_STATS } from '@/frontend/type-it-up-graphql';
-import { get } from 'http';
+import {
+  DailyStats,
+  GET_USER_STATS,
+  UserGameStats,
+} from '@/frontend/type-it-up-graphql';
 
 @Component({
   selector: 'lib-type-it-up-profile',
@@ -17,10 +20,20 @@ import { get } from 'http';
 export class TypeItUpProfileComponent implements OnInit {
   authService = inject(AuthService);
   apollo = inject(Apollo);
+  userStats: UserGameStats | undefined;
+  dailyStats: DailyStats[] = [];
+  user = {
+    username: '',
+    averageWpm: 0,
+    averageAccuracy: 0,
+    highestScore: 0,
+  };
 
   ngOnInit() {
     this.authService.user$.subscribe((user) => {
-      console.log(user);
+      if (user) {
+        this.user.username = user.username;
+      }
     });
     this.getUserStats();
   }
@@ -31,7 +44,22 @@ export class TypeItUpProfileComponent implements OnInit {
         query: GET_USER_STATS,
       })
       .subscribe((result) => {
-        console.log(result);
+        this.user = {
+          ...this.user,
+          averageWpm:
+            Math.round(result.data.getUserStats.averageWpm * 100) / 100,
+          averageAccuracy:
+            Math.round(result.data.getUserStats.averageAccuracy * 100) / 100,
+          highestScore:
+            Math.round(result.data.getUserStats.highestScore * 100) / 100,
+        };
+        this.dailyStats = result.data.getUserStats.dailyStats.map(
+          (stat: DailyStats) => ({
+            ...stat,
+            wpm: Math.round(stat.averageWpm * 100) / 100,
+            accuracy: Math.round(stat.averageAccuracy * 100) / 100,
+          })
+        );
       });
   }
 }
--- libs/frontend/type-it-up-profile/src/lib/type-it-up-user/type-it-up-user.component.ts
+++ libs/frontend/type-it-up-profile/src/lib/type-it-up-user/type-it-up-user.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'lib-type-it-up-user',
   standalone: true,
   imports: [],
   templateUrl: './type-it-up-user.component.html',
-  styleUrl: './type-it-up-user.component.css'
+  styleUrl: './type-it-up-user.component.css',
 })
 export class TypeItUpUserComponent {
-  
+  @Input() user = {
+    username: '',
+    averageWpm: 0,
+    averageAccuracy: 0,
+    highestScore: 0,
+  };
 }
